46de44021c796445ceb58873328ae7353469b80e
Dave Garwacke <admin+git@ifyoumakeit.com>
2016-03-05 08:48:57 -0500

    Add date parser

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ var Handlebars = require("handlebars");
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+
+  // Only show year.
+  ['work', 'volunteer', 'education'].forEach(function(key){
+    if(resume[key]){
+      resume[key] = resume[key].map(function(obj){
+        ['startDate', 'endDate'].forEach(function(date){
+          if(obj[date]){
+            obj[date] = obj[date].substring(0, 4);
+          }
+        });
+        return obj;
+      });
+    }
+  });
+
 	return Handlebars.compile(tpl)({
 		css: css,
 		resume: resume
